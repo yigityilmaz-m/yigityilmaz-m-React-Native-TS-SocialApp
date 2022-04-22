f88aeb6b7878695cc8e5c79169c1bcf64dff4acb
yigityilmaz-m <yigityilmaz.m@gmail.com>
2022-04-22 11:43:23 +0300

    RNCamera implemented

--- src/navigation/SwipeNavigation.tsx
+++ src/navigation/SwipeNavigation.tsx
@@ -1,19 +1,28 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Messages from '../screens/Messages';
 import HomeStack from './FeedStack.tsx/HomeStack';
 import Camera from '../screens/Camera';
-
+import {
+  getFocusedRouteNameFromRoute,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {StackParams} from '../../App';
+type Props = StackScreenProps<StackParams>;
 const SwipeNavigation = () => {
   const Tab = createMaterialTopTabNavigator();
+  const navigation = useNavigation();
+  const route = useRoute();
+
   return (
     <Tab.Navigator initialRouteName="HomeStack" tabBar={() => null}>
       <Tab.Screen name="Camera" component={Camera} />
       <Tab.Screen name="HomeStack" component={HomeStack} />
       <Tab.Screen name="Messages" component={Messages} />
-      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
     </Tab.Navigator>
   );
 };
--- src/screens/Camera.tsx
+++ src/screens/Camera.tsx
@@ -1,16 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {RNCamera} from 'react-native-camera';
+import {useCamera} from 'react-native-camera-hooks';
 
 type Props = {};
 
 const Camera = (props: Props) => {
+  const [{cameraRef}, {takePicture}] = useCamera();
   return (
-    <View>
-      <Text>Camera</Text>
+    <View style={{flex: 1}}>
+      
+      <RNCamera
+        ref={cameraRef}
+        type={RNCamera.Constants.Type.back}
+        style={styles.preview}>
+        <Button title="Photo" onPress={() => {}}></Button>
+      </RNCamera>
     </View>
   );
 };
 
 export default Camera;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  preview: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
